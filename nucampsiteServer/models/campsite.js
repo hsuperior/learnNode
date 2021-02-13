e301491f54e1a32ee72b22572084a8124ad57143
Wilson Hsu <hsu.wilson453@gmail.com>
2021-02-13 09:59:07 -0500

    mongoose population

--- nucampsiteServer/models/campsite.js
+++ nucampsiteServer/models/campsite.js
@@ -16,8 +16,10 @@ const commentSchema = new Schema(
       required: true,
     },
     author: {
-      type: String,
-      required: true,
+      /* type: String,
+      required: true, */
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
   },
   {
