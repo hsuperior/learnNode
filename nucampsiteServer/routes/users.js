837c3272d352e2e7225702c08fc0753754bf6306
Wilson Hsu <hsu.wilson453@gmail.com>
2021-02-20 08:15:27 -0500

    CORS

--- nucampsiteServer/routes/users.js
+++ nucampsiteServer/routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const User = require("../models/user");
 const passport = require("passport");
 const router = express.Router();
+const cors = require("./cors");
 const authenticate = require("../authenticate");
 
 /* GET users listing. */
@@ -11,6 +12,7 @@ const authenticate = require("../authenticate");
 
 router.get(
   "/",
+  cors.corsWithOptions,
   authenticate.verifyUser,
   authenticate.verifyAdmin,
   (req, res, next) => {
@@ -25,7 +27,7 @@ router.get(
   }
 );
 
-router.post("/signup", (req, res) => {
+router.post("/signup", cors.corsWithOptions, (req, res) => {
   User.register(
     new User({ username: req.body.username }),
     req.body.password,
@@ -59,19 +61,24 @@ router.post("/signup", (req, res) => {
   );
 });
 
-router.post("/login", passport.authenticate("local"), (req, res) => {
-  const token = authenticate.getToken({ _id: req.user._id });
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.json({
-    success: true,
-    token: token,
-    status: "You are successfully logged in!",
-  });
-  //console.log("Is this an admin: ", req.user.admin);
-});
+router.post(
+  "/login",
+  cors.corsWithOptions,
+  passport.authenticate("local"),
+  (req, res) => {
+    const token = authenticate.getToken({ _id: req.user._id });
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json({
+      success: true,
+      token: token,
+      status: "You are successfully logged in!",
+    });
+    //console.log("Is this an admin: ", req.user.admin);
+  }
+);
 
-router.get("/logout", (req, res, next) => {
+router.get("/logout", cors.corsWithOptions, (req, res, next) => {
   if (req.session) {
     req.session.destroy();
     res.clearCookie("session-id");
